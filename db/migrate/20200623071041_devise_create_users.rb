ecb27d788ad606a36176459596b3446823933885
NJL7502L <g.hana1211.tako@gmail.com>
2020-06-23 16:14:25 +0900

    JWTで使うカラムを追加
    https://www.nightswinger.dev/2020/02/jwt-authentication-for-rails/
    を参考に

--- db/migrate/20200623071041_devise_create_users.rb
+++ db/migrate/20200623071041_devise_create_users.rb
@@ -34,11 +34,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
 
       t.timestamps null: false
+
+      t.string :jti, null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+
+    add_index :users, :jti, unique: true
   end
 end
